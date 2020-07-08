c73085d3172f7daf2ff28af492bdc6e59ac7b575
Hunter Kvalevog <hunterkva@gmail.com>
2020-07-08 17:42:52 -0500

    Add mc!scoreboard

--- src/main/java/net/hkva/discord/DiscordCommandManager.java
+++ src/main/java/net/hkva/discord/DiscordCommandManager.java
@@ -1,9 +1,12 @@
 package net.hkva.discord;
 
 import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.arguments.ArgumentType;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
+import com.mojang.brigadier.builder.RequiredArgumentBuilder;
 import net.dv8tion.jda.api.entities.Message;
 import net.hkva.discord.discordcommand.PlayersCommand;
+import net.hkva.discord.discordcommand.ScoreboardCommand;
 
 public class DiscordCommandManager {
 
@@ -11,10 +14,15 @@ public class DiscordCommandManager {
 
     public DiscordCommandManager() {
         PlayersCommand.register(dispatcher);
+        ScoreboardCommand.register(dispatcher);
     }
 
     public static LiteralArgumentBuilder<Message> literal(String literal) {
         return LiteralArgumentBuilder.literal(literal);
+    }
+
+    public static <T> RequiredArgumentBuilder<Message, T> argument(String name, ArgumentType<T> type) {
+        return RequiredArgumentBuilder.argument(name, type);
     }
 
     public CommandDispatcher<Message> getDispatcher() {
